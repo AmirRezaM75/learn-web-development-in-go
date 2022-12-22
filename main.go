611548032f991f40e7d357815c76898aa46df5a0
Amir Reza Mehbakhsh <theamirrezam75@gmail.com>
2022-12-22 12:11:37 +0330

    Path handler

--- main.go
+++ main.go
@@ -5,12 +5,29 @@ import (
 	"net/http"
 )
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprint(w, "<h1>Hello World</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, _ = fmt.Fprint(w, "<h1>Home Page</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, _ = fmt.Fprint(w, "<h1>Contact Page</h1>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		//TODO: 404 page
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handleFunc)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Listening on port 3000")
 	_ = http.ListenAndServe(":3000", nil)
 }
