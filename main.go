c91c411f46f18ea4e3478f4eb9f1ac6a2642f319
Amir Reza Mehbakhsh <theamirrezam75@gmail.com>
2022-12-29 11:50:13 +0330

    Render home page with template

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	_, _ = fmt.Fprint(w, "<h1>Home Page</h1>")
+	t, err := template.ParseFiles("templates/home.html")
+
+	if err != nil {
+		log.Printf("parsing template %v", err)
+		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
+		return
+	}
+
+	_ = t.Execute(w, nil)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
