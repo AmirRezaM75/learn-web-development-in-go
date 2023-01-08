ca82eeeb03da67c2091cbc8de8d2203f9fe39375
Amir Reza Mehbakhsh <theamirrezam75@gmail.com>
2023-01-08 22:30:39 +0330

    Connect to postgres

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,14 +1,28 @@
 package controllers
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"net/http"
 )
 
 type UserController struct{}
 
 func (uc UserController) Store(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=root password=root dbname=unsplash sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, "Connected!")
+	return
+
+	err = r.ParseForm()
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
